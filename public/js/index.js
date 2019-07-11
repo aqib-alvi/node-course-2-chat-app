74423f2b110d7b9d23c15dab2bfb679049cef7c3
Aqib <aqib259alvi@gmail.com>
2019-07-11 06:41:03 -0400

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -29,11 +29,13 @@ socket.on('newLocationMessage', function (message) {
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
 
+    let messageTextbox = jQuery('[name=message');
+
     socket.emit('sendMessage', {
         from: 'User',
-        text: jQuery('[name=message').val()
+        text: messageTextbox.val()
     }, function () {
-
+        messageTextbox.val('');
     });
 });
 
@@ -44,13 +46,17 @@ locationButton.on('click', function () {
         return alert('Geolocation not supported by your browser');
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
-            longitude: position.coords.longitude
+            longitude: position.coords.longitude 
         });
 
     }, function () {
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location.');
     })
 });
